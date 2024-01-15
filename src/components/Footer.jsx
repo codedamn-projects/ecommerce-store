affbb2a9fa537313cac8f0ceb1c138304b4d76c4
Aayush Jaiswal <aay7ush@gmail.com>
2024-01-15 20:00:41 +0530

    feat: build header and footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,26 @@
-const Footer = () => {
-  return <div>Footer</div>
-}
+import { Facebook, Instagram, Twitter } from "lucide-react"
+import { Link } from "react-router-dom"
+
+const Footer = () => (
+  <footer className="py-5 border-t">
+    <div className="max-w-7xl mx-auto px-2.5 md:px-7 flex justify-between items-center">
+      <p className="text-lg font-medium">
+        &copy; Copyright {new Date().getFullYear()}. All Rights Reserved.
+      </p>
+
+      <div className="flex gap-3">
+        <Link to="/" className="bg-black p-2 rounded-lg">
+          <Instagram size={25} color="white" />
+        </Link>
+        <Link to="/" className="bg-black p-2 rounded-lg">
+          <Facebook size={25} color="white" />
+        </Link>
+        <Link to="/" className="bg-black p-2 rounded-lg">
+          <Twitter size={25} color="white" />
+        </Link>
+      </div>
+    </div>
+  </footer>
+)
+
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,26 @@
+import { ShoppingCart } from "lucide-react"
+import { Link } from "react-router-dom"
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="shadow border-b py-3">
+      <div className="max-w-7xl mx-auto px-2.5 md:px-7 flex justify-between items-center">
+        <Link to="/">
+          <h1 className="text-2xl font-bold">Store</h1>
+        </Link>
+
+        <div className="relative">
+          <Link to="/">
+            <ShoppingCart size={35} />
+          </Link>
+
+          <p className="bg-black text-white font-medium w-5 h-5 rounded-full grid place-content-center absolute -right-2 -top-1">
+            0
+          </p>
+        </div>
+      </div>
+    </header>
+  )
 }
+
 export default Header
