c0b880fa1a8b5b2d2b6fd6b0d64c600a8f123911
Aayush Jaiswal <aay7ush@gmail.com>
2024-01-19 05:22:02 +0530

    refactor: add styles in index.css file

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -21,17 +21,18 @@ const ProductDetail = () => {
   return (
     <>
       {product ? (
-        <article className="max-w-7xl mx-auto p-2.5 md:px-7 py-10 grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-0 items-center justify-center justify-items-center">
+        <article className="product-detail">
           <section>
             <img
               src={product.image}
               alt="product image"
               width={300}
               height={300}
+              className="product-detail__image"
             />
           </section>
           <section className="space-y-5">
-            <h2 className="text-4xl font-semibold">{product.title}</h2>
+            <h2 className="product-detail__title">{product.title}</h2>
 
             <div className="flex gap-3 font-medium">
               <p>{Math.ceil(product.rating.rate)}</p>
@@ -50,12 +51,11 @@ const ProductDetail = () => {
               <p>| {product.rating.count} ratings</p>
             </div>
 
-            <p>{product.description}</p>
-            <p className="text-4xl font-bold">$ {product.price.toFixed(2)}</p>
-            <button
-              className="flex gap-3 w-full justify-center bg-black transition duration-200 hover:bg-opacity-85 text-white py-2 rounded-lg"
-              onClick={addToCart}
-            >
+            <p className="product-detail__description">{product.description}</p>
+            <p className="product-detail__price">
+              $ {product.price.toFixed(2)}
+            </p>
+            <button className="product-detail__addToCart" onClick={addToCart}>
               <ShoppingCart size={25} />
               <p>Add to Cart</p>
             </button>
